d1765ed6f088c28231f5c14e68a03ee0af9bae92
zuhno <zuhno11@gmail.com>
2024-12-08 23:50:51 +0900

    feat: dayjs plugin into window._dayjs object chore: jsconfig.json

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,16 @@
+//* crypto-js
 import CryptoJS from "crypto-js";
-import lodash from "lodash";
-import dayjs from "dayjs";
+window._cryptojs = CryptoJS;
 
-window.CryptoJS = CryptoJS;
-window.lodash = lodash;
-window.dayjs = dayjs;
+//* lodash
+import lodash from "lodash";
+window._lodash = lodash;
+
+//* dayjs
+import dayjs from "dayjs";
+import dayjs_utc from "dayjs/plugin/utc";
+import dayjs_timezone from "dayjs/plugin/timezone";
+window._dayjs = dayjs;
+window._dayjs.plugins = {};
+window._dayjs.plugins.utc = dayjs_utc;
+window._dayjs.plugins.timezone = dayjs_timezone;
